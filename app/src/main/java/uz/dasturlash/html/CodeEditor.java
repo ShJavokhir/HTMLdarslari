d6d024cddf840e1cd9044588dd52ee51a139e5b5
Javohir Developer <javohirshomuradov@gmail.com>
2019-01-27 00:38:25 +0500

    updates3

--- app/src/main/java/uz/dasturlash/html/CodeEditor.java
+++ app/src/main/java/uz/dasturlash/html/CodeEditor.java
@@ -3,11 +3,25 @@ package uz.dasturlash.html;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class CodeEditor extends AppCompatActivity {
+    Button check;
+    CodeEditText code;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.code_editor);
+        check = (Button) findViewById(R.id.check);
+        code = (CodeEditText) findViewById(R.id.editCode);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("noob",String.valueOf(code.getText()));
+            }
+        });
     }
 }
